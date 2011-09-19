2679f3f6d52e2762701cfc779e92cb6d0390dfb8
Jose Fernandez <jose@newrelic.com>
2011-09-19 00:19:25 -0700

    More work on the newrelic.js backbone code + adds a jasmine test suite

--- NewRelic.safariextension/newrelic.js
+++ NewRelic.safariextension/newrelic.js
@@ -2,21 +2,62 @@
   
   window.Application = Backbone.Model.extend({
     defaults: {
-      apdex: "-",
-      throughput: "-",
-      errors: "-",
-      responseTime: "-"
+      accountId:    "",
+      appId:        "",
+      apiKey:       "",
+      apdex:        "-",
+      throughput:   "-",
+      errorRate:    "-",
+      responseTime: "-",
+      host:         "https://rpm.newrelic.com"
     },
     
     url: function() {
-      return "https://rpm.newrelic.com/accounts/" + this.get("accountId") + "/applications/" + this.get("appId") + "/threshold_values.xml";
+      return this.get("host") + "/accounts/" + this.get("accountId") + "/applications/" + this.get("appId") + "/threshold_values.xml?api_key=" + this.get("apiKey");
     },
     
-    refresh: function() {
-      this.fetch({ headers: { "x-api-key": this.get("apiKey") }});
+    parse: function(response) {
+      metrics = {}
+      $(response).find("threshold_value").each(function() {
+        var formattedValue = $(this).attr('formatted_metric_value');
+        switch($(this).attr('name')) {
+        case "Apdex":
+          metrics['apdex'] = formattedValue;
+          break;
+        case "Throughput":
+          metrics['throughput'] = formattedValue;
+          break;
+        case "Error Rate":
+          metrics['errorRate'] = formattedValue;
+          break;
+        case "Response Time":
+          metrics['responseTime'] = formattedValue;
+          break;
+        }
+      });
+      return metrics;
+    },
+    
+    hasCredentials: function() {
+      return (this.get("accountId").length > 0 && this.get("appId").length > 0 && this.get("apiKey").length > 0);
+    }
+  });
+  
+  window.ApplicationView = Backbone.View.extend({
+    tagName: 'ul',
+    id: 'metrics',
+    
+    initialize: function() {
+      _.bindAll(this, 'render');
+      this.model.bind('change', this.render);
+      this.template = _.template($('#metrics-template').html());
+    },
+    
+    render: function() {
+      var renderedContent = this.template(this.model.toJSON());
+      $(this.el).html(renderedContent);
+      return this;
     }
   });
   
 })(jQuery);
-
-app = new Application({ accountId: "", appId: "", apiKey: "" });
--- test-js/spec/newrelic-spec.js
+++ test-js/spec/newrelic-spec.js
@@ -1,261 +1,72 @@
+var xml = $('#xml-response').html();
+
 describe("Application", function() {
   beforeEach(function () {
     this.application = new Application({
       accountId: "123",
-      appId: "456"
+      appId: "456",
+      apiKey: "key123456890"
     });
-  });
-  
-  it("generates the correct url", function() {
-    expect(this.application.url()).toEqual("https://rpm.newrelic.com/accounts/123/applications/456/threshold_values.xml");
   });
   
   it("sets '-' as the metric defaults", function() {
     expect(this.application.get("apdex")).toEqual("-");
     expect(this.application.get("throughput")).toEqual("-");
-    expect(this.application.get("errors")).toEqual("-");
+    expect(this.application.get("errorRate")).toEqual("-");
     expect(this.application.get("responseTime")).toEqual("-");
   });
+  
+  describe("url()", function() {
+    it("generates the correct url", function() {
+      expect(this.application.url()).toEqual("https://rpm.newrelic.com/accounts/123/applications/456/threshold_values.xml?api_key=key123456890");
+    });
+    
+    it("uses a custom host", function() {
+      this.application.set({ host: "http://localhost:3000" });
+      expect(this.application.url()).toEqual("http://localhost:3000/accounts/123/applications/456/threshold_values.xml?api_key=key123456890");
+    });
+  });
+  
+  describe("parse() with a metrics XML", function() {
+    it("returns an object with the apdex value", function() {
+      var object = this.application.parse(xml);
+      expect(object["apdex"]).toEqual("0.96 [1.0]*");
+    });
+    
+    it("returns an object with the errorRate value", function() {
+      var object = this.application.parse(xml);
+      expect(object["errorRate"]).toEqual("0.05%");
+    });
+    
+    it("returns an object with the throughput value", function() {
+      var object = this.application.parse(xml);
+      expect(object["throughput"]).toEqual("345 rpm");
+    });
+    
+    it("returns an object with the responseTime value", function() {
+      var object = this.application.parse(xml);
+      expect(object["responseTime"]).toEqual("150 ms");
+    });
+  });
+  
+  describe("hasCredentials()", function() {
+    it("returns true when accountId, appId, and apiKey are present", function() {
+      expect(this.application.hasCredentials()).toBeTruthy();
+    });
+    
+    it("returns false when accountId is blank", function() {
+      this.application.set({ accountId: "" });
+      expect(this.application.hasCredentials()).toBeFalsy();
+    });
+    
+    it("returns false when appId is blank", function() {
+      this.application.set({ appId: "" });
+      expect(this.application.hasCredentials()).toBeFalsy();
+    });
+    
+    it("returns false when apiKey is blank", function() {
+      this.application.set({ apiKey: "" });
+      expect(this.application.hasCredentials()).toBeFalsy();
+    });
+  });
 });
-
-// var albumData = [{
-//     "title":  "Album A",
-//     "artist": "Artist A",
-//     "tracks": [
-//         {
-//             "title": "Track A",
-//             "url": "/music/Album A Track A.mp3"
-//         },
-//         {
-//             "title": "Track B",
-//             "url": "/music/Album A Track B.mp3"
-//         }]
-// }, {
-//     "title": "Album B",
-//     "artist": "Artist B",
-//     "tracks": [
-//         {
-//             "title": "Track A",
-//             "url": "/music/Album B Track A.mp3"
-//         },
-//         {
-//             "title": "Track B",
-//             "url": "/music/Album B Track B.mp3"
-//     }]
-// }];
-// 
-// describe("Album", function () {
-// 
-//     beforeEach(function () {
-//         this.album = new Album(albumData[0]);
-//     });
-// 
-//     it("creates from data", function () {
-//         expect(this.album.get('tracks').length).toEqual(2);
-//     });
-// 
-//     describe("first track", function () {
-// 
-//         it("returns true for first track", function () {
-//             expect(this.album.isFirstTrack(0)).toBeTruthy();
-//         });
-// 
-//         it("returns false for other tracks", function () {
-//             expect(this.album.isFirstTrack(12)).toBeFalsy();
-//         });
-// 
-//     });
-// 
-//     describe("last track", function () {
-// 
-//         it("returns true for last track", function () {
-//             expect(this.album.isLastTrack(1)).toBeTruthy();
-//         });
-// 
-//         it("returns false for other tracks", function () {
-//             expect(this.album.isLastTrack(0)).toBeFalsy();
-//         });
-// 
-//     });
-// 
-//     describe("track url at index", function () {
-// 
-//         it("returns URL for existing track", function () {
-//             expect(this.album.trackUrlAtIndex(0))
-//                 .toEqual('/music/Album A Track A.mp3');
-//         });
-// 
-//         it("returns null for non-existing track", function () {
-//             expect(this.album.trackUrlAtIndex(5)).toBe(null);
-//         });
-// 
-//     });
-// 
-// });
-// 
-// describe("Playlist", function() {
-//   
-//   beforeEach(function() {
-//       this.playlist = new Playlist();
-//       this.playlist.add(albumData[0]);
-//   });
-//   
-//   it("has models", function () {
-//       expect(this.playlist.models.length).toEqual(1);
-//   });
-//   
-//   it("identifies first album as first", function() {
-//       expect(this.playlist.isFirstAlbum(0)).toBeTruthy();
-//   });
-// 
-//   it("rejects non-first album as first", function() {
-//       expect(this.playlist.isFirstAlbum(1)).toBeFalsy();
-//   });
-// 
-//   it("identifies last album as last", function() {
-//       this.playlist.add(albumData[1]); // Extra album as the last
-//       expect(this.playlist.isLastAlbum(1)).toBeTruthy();
-//   });
-// 
-//   it("rejects non-last album as last", function() {
-//       this.playlist.add(albumData[1]); // Extra album as the last
-//       expect(this.playlist.isLastAlbum(0)).toBeFalsy();
-//   });
-//     
-// });
-// 
-// describe("Player", function () {
-// 
-//     describe("with no items", function () {
-// 
-//         beforeEach(function () {
-//             this.player = new Player();
-//         });
-// 
-//         it("starts with album 0", function () {
-//             expect(this.player.get('currentAlbumIndex')).toEqual(0);
-//         });
-// 
-//         it("starts with track 0", function () {
-//             expect(this.player.get('currentTrackIndex')).toEqual(0);
-//         });
-// 
-//     });
-// 
-//     describe("with added album", function () {
-// 
-//         beforeEach(function () {
-//             this.player = new Player();
-//             this.player.playlist.add(albumData[0]);
-//         });
-// 
-//         it("is in 'stop' state", function () {
-//             expect(this.player.get('state')).toEqual('stop');
-//         });
-//         
-//         it("is stopped", function() {
-//             expect(this.player.isStopped()).toBeTruthy();
-//         });
-// 
-//         describe("while playing", function () {
-// 
-//             beforeEach(function () {
-//                 this.player.play();
-//             });
-// 
-//             it("sets to 'play' state", function () {
-//                 expect(this.player.get('state')).toEqual('play');
-//             });
-// 
-//             it("is playing", function() {
-//                 expect(this.player.isPlaying()).toBeTruthy();
-//             });
-// 
-//             it("has a current album", function () {
-//                 expect(this.player.currentAlbum().get('title'))
-//                     .toEqual('Album A');
-//             });
-// 
-//             it("has a current track URL", function () {
-//                 expect(this.player.currentTrackUrl())
-//                     .toEqual("/music/Album A Track A.mp3");
-//             });
-// 
-//             it("pauses", function() {
-//                 this.player.pause();
-//                 expect(this.player.get('state')).toEqual('pause');
-//             });
-// 
-//             describe("next track", function () {
-// 
-//                 beforeEach(function() {
-//                     // Add extra album to test 'next'
-//                     this.player.playlist.add(albumData[1]);
-//                 });
-// 
-//                 it("increments within an album", function () {
-//                     this.player.nextTrack();
-// 
-//                     expect(this.player.get('currentAlbumIndex')).toEqual(0);
-//                     expect(this.player.get('currentTrackIndex')).toEqual(1);
-//                 });
-// 
-//                 it("goes to the next album", function () {
-//                     this.player.set({'currentTrackIndex': 1}); // Last track
-//                     this.player.nextTrack();
-//                     
-//                     expect(this.player.get('currentAlbumIndex')).toEqual(1);
-//                     expect(this.player.get('currentTrackIndex')).toEqual(0);
-//                 });
-// 
-//                 it("wraps around to the first album if at end", function () {
-//                     this.player.set({'currentAlbumIndex': 1}); // Last album
-//                     this.player.set({'currentTrackIndex': 1}); // Last track
-//                     this.player.nextTrack();
-//                     
-//                     expect(this.player.get('currentAlbumIndex')).toEqual(0);
-//                     expect(this.player.get('currentTrackIndex')).toEqual(0);
-//                 });
-// 
-//             });
-// 
-//             describe("previous track", function () {
-// 
-//                 beforeEach(function() {
-//                     // Add extra album to test 'prev'
-//                     this.player.playlist.add(albumData[1]);
-//                 });
-// 
-//                 it("goes to the previous track in an album", function () {
-//                     this.player.set({'currentTrackIndex': 1});
-//                     this.player.prevTrack();
-//                     
-//                     expect(this.player.get('currentAlbumIndex')).toEqual(0);
-//                     expect(this.player.get('currentTrackIndex')).toEqual(0);
-//                 });
-//             
-//                 it("goes to the last track of previous album", function() {
-//                     this.player.set({'currentAlbumIndex': 1});
-//                     this.player.set({'currentTrackIndex': 0});
-//                     this.player.prevTrack();
-//                     
-//                     expect(this.player.get('currentAlbumIndex')).toEqual(0);
-//                     expect(this.player.get('currentTrackIndex')).toEqual(1);
-//                 });
-//                 
-//                 it("wraps around if at the very beginning", function() {
-//                     this.player.set({'currentAlbumIndex': 0});
-//                     this.player.set({'currentTrackIndex': 0});
-//                     this.player.prevTrack();
-//                     
-//                     expect(this.player.get('currentAlbumIndex')).toEqual(1);
-//                     expect(this.player.get('currentTrackIndex')).toEqual(1);
-//                 });
-//             
-//             });
-// 
-//         });
-// 
-//     });
-// 
-// });
